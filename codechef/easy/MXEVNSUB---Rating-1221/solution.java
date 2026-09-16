25151594db11a73c263fc2aac3453ffb3a4b3a54
Rishabh Dubey <rishabhdubey1472004@gmail.com>
2026-09-16 21:15:16 +0530

    ✅ MXEVNSUB - Rating 1221 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/easy/MXEVNSUB---Rating-1221/solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CodeChef{
+    	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t-- > 0 ){
+		    
+		    int n = sc.nextInt();
+            
+            int sum = n * ( n + 1 ) /2;
+            
+            if( (sum&1) == 0 ){
+                System.out.println( n );
+            }
+            else{
+                System.out.println( n - 1 );
+            }
+		    
+		}
+	}
+}
